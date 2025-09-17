62c062312c8d9dc2c134053a5a409437c7fe2c8a
Ankit Gupta <ankgupta49@WKMZTBF5D158.global.publicisgroupe.net>
2025-09-17 13:36:33 +0530

    removes divide function

--- app.js
+++ app.js
@@ -10,10 +10,5 @@ function substract(a, b) {
   return a - b;
 }
 
-function divide(a, b) {
-  return a / b;
-}
-
 const sumVal = sum(2, 3);
-const divideVal = divide(10, 2);
-console.log(sumVal, divideVal);
+console.log(sumVal);
